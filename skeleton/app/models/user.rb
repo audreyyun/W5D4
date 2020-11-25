c3a72cfd101bd270792d173c3eb16ee302849d3c
audreyyun <96audreyyun@gmail.com>
2020-11-25 15:46:16 -0500

    Finish models

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+    has_many :enrollments,
+    class_name: :Enrollment,
+    foreign_key: :enrollment_id,
+    primary_key: :id
+
+    has_many :enrolled_course, 
+    through: :enrollments, #only has access to method names
+    source:  :course
+
 end
